48d1d94f984766ef473c4d8ce04886f4c10de7c0
AndyChiquin <andychiquindev@gmail.com>
2025-06-19 15:15:58 -0500

    update: db.js

--- Backend/product_domain/catalogService/src/config/db.js
+++ Backend/product_domain/catalogService/src/config/db.js
@@ -1,12 +1,15 @@
-// src/config/db.js
 const AWS = require('aws-sdk');
 require('dotenv').config();
 
+// 🔒 Forzar las credenciales falsas antes de instanciar el cliente
+AWS.config.update({
+  accessKeyId: 'fakeMyKeyId',
+  secretAccessKey: 'fakeSecretAccessKey',
+  region: process.env.AWS_REGION
+});
+
 const dynamoClient = new AWS.DynamoDB.DocumentClient({
-  region: process.env.AWS_REGION,                  // Obligatorio para que el SDK funcione
-  endpoint: process.env.DYNAMODB_LOCAL_ENDPOINT,   // Conectado al contenedor local
-  accessKeyId: 'dummy-access-key',                 // Falso, pero requerido por el SDK
-  secretAccessKey: 'dummy-secret-key'
+  endpoint: process.env.DYNAMODB_LOCAL_ENDPOINT
 });
 
 const TABLE_NAME = process.env.DYNAMO_TABLE_NAME;
