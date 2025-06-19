2aa07ca44f3f4463c0d0f7d03fb888f05add9335
AndyChiquin <andychiquindev@gmail.com>
2025-06-19 18:48:37 -0500

    update: update db.js

--- Backend/product_domain/inventoryService/src/config/db.js
+++ Backend/product_domain/inventoryService/src/config/db.js
@@ -1,16 +1,12 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  password: process.env.DB_PASSWORD, // aquí estaba mal escrito en tu código, usaste DB_PASS
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
-db.connect(err => {
-  if (err) throw err;
-  console.log('✅ Conectado a MariaDB desde inventoryService');
-});
-
-module.exports = db;
+module.exports = pool;
