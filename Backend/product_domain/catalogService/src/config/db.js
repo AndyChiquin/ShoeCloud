6f135df3849a938cca010375b7160ee9c669816e
AndyChiquin <andychiquindev@gmail.com>
2025-06-19 15:05:42 -0500

    update: db.js

--- Backend/product_domain/catalogService/src/config/db.js
+++ Backend/product_domain/catalogService/src/config/db.js
@@ -1,10 +1,11 @@
+// src/config/db.js
 const AWS = require('aws-sdk');
 require('dotenv').config();
 
 const dynamoClient = new AWS.DynamoDB.DocumentClient({
-  region: process.env.AWS_REGION,
-  endpoint: process.env.DYNAMODB_LOCAL_ENDPOINT,
-  accessKeyId: 'dummy-access-key',
+  region: process.env.AWS_REGION,                  // Obligatorio para que el SDK funcione
+  endpoint: process.env.DYNAMODB_LOCAL_ENDPOINT,   // Conectado al contenedor local
+  accessKeyId: 'dummy-access-key',                 // Falso, pero requerido por el SDK
   secretAccessKey: 'dummy-secret-key'
 });
 
