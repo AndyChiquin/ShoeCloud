b4403089893aad79235bf1bc37be0e25d9929dec
AndyChiquin <andychiquindev@gmail.com>
2025-06-19 21:08:15 -0500

    update: update app.js

--- Backend/product_domain/categoryService/src/app.js
+++ Backend/product_domain/categoryService/src/app.js
@@ -15,3 +15,27 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`CategoryService running on port ${PORT}`);
 });
+
+const { CreateTableCommand } = require("@aws-sdk/client-dynamodb");
+const client = require('./src/config/dynamoClient');
+
+const createTableIfNotExists = async () => {
+  try {
+    const command = new CreateTableCommand({
+      TableName: "Categories",
+      KeySchema: [{ AttributeName: "id", KeyType: "HASH" }],
+      AttributeDefinitions: [{ AttributeName: "id", AttributeType: "S" }],
+      ProvisionedThroughput: { ReadCapacityUnits: 5, WriteCapacityUnits: 5 }
+    });
+    await client.send(command);
+    console.log("✅ Table 'Categories' created.");
+  } catch (err) {
+    if (err.name !== "ResourceInUseException") {
+      console.error("❌ Table creation failed:", err.message);
+    } else {
+      console.log("⚠️ Table already exists.");
+    }
+  }
+};
+
+createTableIfNotExists();
