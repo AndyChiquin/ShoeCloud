f836da63341e91283027f1da006165019c018747
AndyChiquin <andychiquindev@gmail.com>
2025-06-22 09:29:30 -0500

    feat: update imageController

--- Backend/product_domain/imageService/src/controllers/imageController.js
+++ Backend/product_domain/imageService/src/controllers/imageController.js
@@ -8,6 +8,12 @@ const { checkProductExists } = require('../services/productService');
 const uploadImage = async (req, res) => {
   const { product_id, image_url } = req.body;
 
+  // 🛑 Validar que el producto exista
+  const exists = await checkProductExists(product_id);
+  if (!exists) {
+    return res.status(404).json({ error: 'Producto no encontrado en catalogService' });
+  }
+
   try {
     const newImage = new Image({ product_id, image_url });
     await newImage.save();
@@ -16,6 +22,7 @@ const uploadImage = async (req, res) => {
     res.status(500).json({ error: 'Error al subir imagen', details: error.message });
   }
 };
+
 
 // Obtener imagen(es) por ID de producto
 const getImagesByProductId = async (req, res) => {
