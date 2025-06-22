d8f02858e78ebafb8881a770ad47ec027d3730bc
AndyChiquin <andychiquindev@gmail.com>
2025-06-22 09:12:55 -0500

    feat: update productServic

--- Backend/product_domain/imageService/src/services/productService.js
+++ Backend/product_domain/imageService/src/services/productService.js
@@ -1,11 +1,16 @@
-// src/services/productService.js
-
 const axios = require('axios');
 
 const checkProductExists = async (product_id) => {
   try {
     const response = await axios.get(`http://54.166.240.10:3000/api/products/${product_id}`);
-    return response.status === 200;
+
+    // ✅ Validar que exista un producto dentro del body
+    if (response.data && response.data.product && response.data.product.id === product_id) {
+      return true;
+    }
+
+    return false;
+
   } catch (error) {
     console.error('⚠️ Error al verificar producto en catalogService:', error.message);
     return false;
