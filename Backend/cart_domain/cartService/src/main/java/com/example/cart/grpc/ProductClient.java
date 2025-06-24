cdb71529d65bbc3d9f166f8ebb54587dfab6b2aa
AndyChiquin <andychiquindev@gmail.com>
2025-06-23 20:40:04 -0500

    chore: remove unused microservice deleteReservation

--- Backend/cart_domain/cartService/src/main/java/com/example/cart/grpc/ProductClient.java
+++
@@ -1,8 +0,0 @@
-package com.example.cart.grpc;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class ProductClient {
-    // Aquí luego pondremos las llamadas gRPC al productService
-}
