49094a4257fad17b818b847db10d3730e33e7c91
AndyChiquin <andychiquindev@gmail.com>
2025-06-26 12:55:46 -0500

    feat: update login_service.py

--- Backend/user_domain/authService/login/app/services/login_service.py
+++ Backend/user_domain/authService/login/app/services/login_service.py
@@ -9,7 +9,6 @@ def login_user(email: str, password: str):
     from app.config.db import SessionLocal
     from app.models.user_model import User
 
-    # 🔍 PRIMERO: intento buscar en el microservicio externo
     response = requests.get(f"{settings.USER_SERVICE_URL}/users/email/{email}")
     if response.status_code == 200:
         user_data = response.json()
@@ -19,32 +18,32 @@ def login_user(email: str, password: str):
 
         token = create_token({"sub": user_data["id"]})
         redis_client.setex(f"token:{user_data['id']}", settings.JWT_EXPIRE_MINUTES * 60, token)
+#
+        # try:
+        #     session_response = requests.post(
+        #         "http://44.218.255.193:8003/session/",
+        #         json={"user_id": user_data["id"]}
+        #     )
+        #     if session_response.status_code != 201:
+        #         return {"success": False, "error": "Session creation failed"}
+        # except Exception as e:
+        #     return {"success": False, "error": f"Session error: {str(e)}"}
 
-        try:
-            session_response = requests.post(
-                "http://44.218.255.193:8003/session/",
-                json={"user_id": user_data["id"]}
-            )
-            if session_response.status_code != 201:
-                return {"success": False, "error": "Session creation failed"}
-        except Exception as e:
-            return {"success": False, "error": f"Session error: {str(e)}"}
-
-        try:
-            audit_response = requests.post(
-                "http://44.218.255.193:8004/log",
-                json={
-                    "user_id": user_data["id"],
-                    "action": "login_success",
-                    "metadata": {
-                        "email": email
-                    }
-                }
-            )
-            if audit_response.status_code != 201:
-                print("Audit log not created:", audit_response.text)
-        except Exception as e:
-            print("AuditService exception:", str(e))
+        # try:
+        #     audit_response = requests.post(
+        #         "http://44.218.255.193:8004/log",
+        #         json={
+        #             "user_id": user_data["id"],
+        #             "action": "login_success",
+        #             "metadata": {
+        #                 "email": email
+        #             }
+        #         }
+        #     )
+        #     if audit_response.status_code != 201:
+        #         print("Audit log not created:", audit_response.text)
+        # except Exception as e:
+        #     print("AuditService exception:", str(e))
 
         return {"success": True, "token": token, "user_id": user_data["id"]}
 
@@ -66,30 +65,30 @@ def login_user(email: str, password: str):
     token = create_token({"sub": user_data["id"]})
     redis_client.setex(f"token:{user_data['id']}", settings.JWT_EXPIRE_MINUTES * 60, token)
 
-    try:
-        session_response = requests.post(
-            "http://44.218.255.193:8003/session/",
-            json={"user_id": user_data["id"]}
-        )
-        if session_response.status_code != 201:
-            return {"success": False, "error": "Session creation failed"}
-    except Exception as e:
-        return {"success": False, "error": f"Session error: {str(e)}"}
+    # try:
+    #     session_response = requests.post(
+    #         "http://44.218.255.193:8003/session/",
+    #         json={"user_id": user_data["id"]}
+    #     )
+    #     if session_response.status_code != 201:
+    #         return {"success": False, "error": "Session creation failed"}
+    # except Exception as e:
+    #     return {"success": False, "error": f"Session error: {str(e)}"}
 
-    try:
-        audit_response = requests.post(
-            "http://44.218.255.193:8004/log",
-            json={
-                "user_id": user_data["id"],
-                "action": "login_success",
-                "metadata": {
-                    "email": email
-                }
-            }
-        )
-        if audit_response.status_code != 201:
-            print("Audit log not created:", audit_response.text)
-    except Exception as e:
-        print("AuditService exception:", str(e))
+    # try:
+    #     audit_response = requests.post(
+    #         "http://44.218.255.193:8004/log",
+    #         json={
+    #             "user_id": user_data["id"],
+    #             "action": "login_success",
+    #             "metadata": {
+    #                 "email": email
+    #             }
+    #         }
+    #     )
+    #     if audit_response.status_code != 201:
+    #         print("Audit log not created:", audit_response.text)
+    # except Exception as e:
+    #     print("AuditService exception:", str(e))
 
     return {"success": True, "token": token, "user_id": user_data["id"]}
