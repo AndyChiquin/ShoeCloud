0c7c422ffe750ceee170d1438c958c1d9e41fc28
AndyChiquin <andychiquindev@gmail.com>
2025-06-27 10:15:42 -0500

    fix: update logout_service

--- Backend/user_domain/authService/logout/app/services/logout_service.py
+++ Backend/user_domain/authService/logout/app/services/logout_service.py
@@ -1,6 +1,7 @@
 import requests
 from app.services.redis_client import redis_client
 from app.utils.jwt_utils import verify_token
+from app.utils.audit_logger import log_user_action_soap  # IMPORTANTE
 
 def logout_user(token: str):
     result = verify_token(token)
@@ -10,6 +11,7 @@ def logout_user(token: str):
     user_id = result["data"]["sub"]
     redis_client.delete(f"token:{user_id}")
 
+    # Cerrar sesión activa
     try:
         response = requests.patch(f"http://3.209.221.173:8012/session/user/{user_id}/close-latest")
         if response.status_code != 200:
@@ -17,18 +19,14 @@ def logout_user(token: str):
     except Exception as e:
         print("SessionService exception:", str(e))
 
+    # Registrar en auditoría usando SOAP
     try:
-        audit_response = requests.post(
-            "http://3.209.221.173:8014/log",
-            json={
-                "user_id": user_id,
-                "action": "logout",
-                "metadata": {}
-            }
+        log_user_action_soap(
+            user_id=user_id,
+            action="logout_success",
+            metadata={"info": "Sesión cerrada correctamente"}
         )
-        if audit_response.status_code != 201:
-            print("Audit log not created:", audit_response.text)
     except Exception as e:
-        print("AuditService exception:", str(e))
+        print("SOAP AuditService exception:", str(e))
 
     return {"success": True}
