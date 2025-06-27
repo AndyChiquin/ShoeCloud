e70c544a9083f172ea9b1d5a16686222e96fb1b4
AndyChiquin <andychiquindev@gmail.com>
2025-06-26 20:44:10 -0500

    fix: update read_route.py

--- Backend/user_domain/userAuditService/readAudit/app/routes/read_route.py
+++ Backend/user_domain/userAuditService/readAudit/app/routes/read_route.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, jsonify
 from app.services.read_service import get_user_logs
 
-# KISS: Organiza rutas de lectura
 read_bp = Blueprint("read_bp", __name__, url_prefix="/audit")
 
 @read_bp.route("/logs/<int:user_id>", methods=["GET"])
