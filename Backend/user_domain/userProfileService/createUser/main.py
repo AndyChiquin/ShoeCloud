9ae1f709aace7a1aef4f47157ef612be3f727f4c
AndyChiquin <andychiquindev@gmail.com>
2025-06-29 16:09:19 -0500

    fix: update files for test in CI for createUser and add deploy.yml

--- Backend/user_domain/userProfileService/createUser/main.py
+++ Backend/user_domain/userProfileService/createUser/main.py
@@ -1,16 +1,25 @@
 from flask import Flask
-from app.config.settings import settings
+import os
+from dotenv import load_dotenv
+
+if os.environ.get("FLASK_ENV") == "testing":
+    from app.config.test_settings import settings
+    load_dotenv(".env.test")
+else:
+    from app.config.settings import settings
+    load_dotenv()
+
 from app.db.connection import db
-from app.routes.user_routes import user_create_bp  
+from app.routes.user_routes import user_create_bp
 
 app = Flask(__name__)
-app.config.from_object(settings)  
+app.config.from_object(settings)
 db.init_app(app)
 
 with app.app_context():
-    db.create_all()  
-app.register_blueprint(user_create_bp)
+    db.create_all()
 
+app.register_blueprint(user_create_bp)
 
 @app.route("/")
 def health_check():
@@ -18,4 +27,3 @@ def health_check():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
-
