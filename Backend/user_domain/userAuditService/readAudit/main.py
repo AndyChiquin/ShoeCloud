0185f087d1c3ed1fe47a9fa1962fe1ad4ccb2eed
AndyChiquin <andychiquindev@gmail.com>
2025-07-01 22:29:35 -0500

    fix: update actions for read and create and read audit

--- Backend/user_domain/userAuditService/readAudit/main.py
+++ Backend/user_domain/userAuditService/readAudit/main.py
@@ -3,14 +3,24 @@ from dotenv import load_dotenv
 from app.routes.read_route import read_bp
 import os
 
-load_dotenv()
+if os.environ.get("FLASK_ENV") == "testing":
+    from app.config.test_settings import TestSettings as Settings
+    load_dotenv(".env.test")
+else:
+    from app.config.config import Settings
+    load_dotenv()
 
 app = Flask(__name__)
+app.config.from_object(Settings)
+app.config["JSON_SORT_KEYS"] = False
 
 app.register_blueprint(read_bp)
+
+@app.route("/")
+def health_check():
+    return {"status": "Audit Service OK"}
 
 PORT = int(os.getenv("PORT", 8004))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=PORT)
-
