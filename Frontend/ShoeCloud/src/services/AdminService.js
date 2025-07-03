1fcd0ab1859b91dbfee0774eaab0456de81fc5a1
AndyChiquin <andychiquindev@gmail.com>
2025-07-03 09:26:24 -0500

    fix: delete folders and files for frontend

--- Frontend/ShoeCloud/src/services/AdminService.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default {
-  async loginAdmin(data) {
-    const response = await axios.post('http://localhost:3001/login', data);
-    return response.data;
-  }
-};
