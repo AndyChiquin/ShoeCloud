37fa7165ca58f599cae171c01c4b845577f28a61
AndyChiquin <andychiquindev@gmail.com>
2025-07-03 12:06:53 -0500

    fix: update main.py for logout and validate for frontend

--- Backend/user_domain/authService/logout/main.py
+++ Backend/user_domain/authService/logout/main.py
@@ -1,6 +1,8 @@
 from flask import Flask
 import os
 from dotenv import load_dotenv
+from flask_cors import CORS
+
 
 if os.environ.get("FLASK_ENV") == "testing":
     from app.config.test_settings import settings
@@ -13,6 +15,8 @@ else:
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False 
 app.config.from_object(settings)
+
+CORS(app)
 
 from app.routes.logout_routes import logout_bp
 app.register_blueprint(logout_bp)
--- Backend/user_domain/authService/validateToken/main.py
+++ Backend/user_domain/authService/validateToken/main.py
@@ -1,6 +1,8 @@
 from flask import Flask
 import os
 from dotenv import load_dotenv
+from flask_cors import CORS
+
 
 if os.environ.get("FLASK_ENV") == "testing":
     from app.config.test_settings import settings
@@ -12,6 +14,9 @@ else:
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False  
 app.config.from_object(settings)
+
+CORS(app)
+
 
 from app.routes.validate_routes import validate_bp
 app.register_blueprint(validate_bp)
