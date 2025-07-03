b9d59c15c168e6bdcc14ffa3a42006255afd59d3
AndyChiquin <andychiquindev@gmail.com>
2025-07-03 14:19:13 -0500

    fix: update user_routes.py

--- Backend/user_domain/userProfileService/createUser/app/routes/user_routes.py
+++ Backend/user_domain/userProfileService/createUser/app/routes/user_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, make_response
 from app.services.user_service import create_user
 from app.models.user_model import User
 
@@ -6,6 +6,9 @@ user_create_bp = Blueprint("user_create", __name__, url_prefix="/users")
 
 @user_create_bp.route("/", methods=["POST", "OPTIONS"])
 def register_user():
+    if request.method == "OPTIONS":
+        return make_response('', 200)
+
     data = request.json
     user = create_user(data)
     return jsonify({"id": user.id, "name": user.name}), 201
