8899af34aae6d2da39edd42c421cf4e9b447d433
AndyChiquin <andychiquindev@gmail.com>
2025-07-04 15:00:17 -0500

    fix: update files for create price

--- Backend/product_domain/pricingService/createPrice/controllers/price_controller.rb
+++ Backend/product_domain/pricingService/createPrice/controllers/price_controller.rb
@@ -5,10 +5,7 @@ class PriceController
   def self.create(data)
     price = Price.create(data)
 
-    if price.persisted?
-      WebSocketClient.send_price_update(price)
-    end
-
+    
     price
   end
 end
