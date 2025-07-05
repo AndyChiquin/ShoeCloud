75c6b19ac229c81aa3204ce6ba595e41b2e9f500
AndyChiquin <andychiquindev@gmail.com>
2025-07-04 19:48:44 -0500

    fix: update files for update product

--- Backend/product_domain/catalogService/updateProduct/src/services/priceNotifier.js
+++ Backend/product_domain/catalogService/updateProduct/src/services/priceNotifier.js
@@ -1,13 +1,17 @@
 const WebSocket = require('ws');
 
 function notifyPricingService(productId, price) {
-  const ws = new WebSocket('ws://34.198.245.223:3020/ws/price'); 
+  console.log('[DEBUG] Enviando a pricingService UPDATE');
+  console.log('productId:', productId);
+  console.log('price:', price);
+
+  const ws = new WebSocket('ws://34.198.245.223:3020/ws/price'); // UPDATE
 
   ws.on('open', () => {
     const payload = {
       action: "update",
       data: {
-        id: productId,
+        id: productId,  // debe llamarse "id" si el microservicio Ruby lo espera así
         price,
         discount_type: "none",
         percentage: 0,
@@ -15,16 +19,18 @@ function notifyPricingService(productId, price) {
       }
     };
 
+    console.log('Payload enviado:', payload);  // 👈 nuevo log
+
     ws.send(JSON.stringify(payload));
-    ws.close(); 
+    ws.close();
   });
 
   ws.on('message', (data) => {
-    console.log('[pricingService - update]:', data.toString());
+    console.log('[pricingService response]:', data.toString());
   });
 
   ws.on('error', (err) => {
-    console.error('[WebSocket Error pricingService - update]:', err.message);
+    console.error('[WebSocket Error]:', err.message);
   });
 }
 
