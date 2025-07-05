f42496461e8e8cdf0fee380d4e5a332b05f2cdc6
AndyChiquin <andychiquindev@gmail.com>
2025-07-04 21:51:59 -0500

    fix: update comments for update and delete category service

--- Backend/product_domain/categoryService/deleteCategory/src/controllers/deleteController.js
+++ Backend/product_domain/categoryService/deleteCategory/src/controllers/deleteController.js
@@ -1,6 +1,5 @@
 const { deleteCategory } = require('../services/deleteService');
 
-// Eliminar una categoría por ID
 const remove = async (req, res) => {
   try {
     await deleteCategory(req.params.id);
